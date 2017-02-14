22e3e9eb0fd0b0d988cad339b9183eae6f543148
Jobsity <jobsity@Jobsitys-Mac-mini.local>
2017-02-14 15:32:52 -0500

    Modified error messages

--- src/components/Attribute/errorMessages.js
+++ src/components/Attribute/errorMessages.js
@@ -3,14 +3,10 @@ import React from 'react';
 export const validationStandardError = <i>Required field</i>;
 export const nameValidationError =
   <i>Required field, attributes names must be unique</i>;
-export const precisionError =
-  (<i>
-     Invalid precision value
-  </i>);
-export const accuracyError =
-  (<i>
-    Invalid accuracy value
-  </i>);
+
+export const precisionError = <i>Invalid precision value</i>;
+
+export const accuracyError = <i>Invalid accuracy value</i>;
 
 export const rangeMinError =
   <i>Invalid value Range min has to be lower than Range max and its required</i>;
