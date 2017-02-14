7763644b36d7c7d011514b2759f429fdeeca9f30
Jobsity <jobsity@Jobsitys-Mac-mini.local>
2017-02-14 15:14:57 -0500

    Name val error message edited

--- src/components/Attribute/errorMessages.js
+++ src/components/Attribute/errorMessages.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const validationStandardError = <i>Required field</i>;
 export const nameValidationError =
-  <i>Required field, attributes names must be unique and cant be empty Strings</i>;
+  <i>Required field, attributes names must be unique</i>;
 export const precisionError =
   (<i>
      Invalid precision value
